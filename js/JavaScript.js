ffbcbb6412d3b51d4472d3a38dc9a993d9993e97
leandro777-cell <leandrosalvatierra73@gmail.com>
2024-11-11 18:03:35 -0300

    Add files via upload

---
+++ js/JavaScript.js
@@ -0,0 +1,25 @@
+
+
+
+
+function darkmode(){ document.body.style.backgroundColor = "black";
+
+    document.body.style.backgroundImage= "none";
+
+    
+    document.getElementById("header").style.backgroundColor = "black";
+    document.getElementById("header").style.color = "#90ffe9";
+  
+    document.getElementById("nav").style.color = "black";
+    document.getElementById("section").style.backgroundColor = "black";
+    document.getElementById("section").style.color = "whitesmoke";
+  
+    document.getElementById("footer").style.backgroundColor = "black";
+}
+
+function  autor()
+{
+    alert("un pibe que le gusta jugar y comer milanesa :D");
+}
+
+
